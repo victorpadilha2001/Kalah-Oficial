67dd8ee63481effe8fa24df4db482a6cbeba0e5a
Arthur Bartol <arthur.b4rtol@gmail.com>
2023-11-13 12:45:12 -0300

    arrumei algumas coisas

--- Kalah/src/kalah.java
+++ Kalah/src/kalah.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kalah {
+public class Kalah {
 
     Scanner scan = new Scanner(System.in);
     Tabuleiro tabuleiro;
@@ -8,8 +8,7 @@ public class kalah {
     boolean vez,repeteVez,fim;  //true = jogador1, false = jogador 2
     int opcao = 0;
     
-    public kalah(){
-
+    public Kalah() {
         tabuleiro = new Tabuleiro();
         int cava = 0;
 
@@ -24,69 +23,60 @@ public class kalah {
         tabuleiro.mostrarTabuleiro();
         System.out.println();
 
-        while(opcao != 2){
+        while (opcao != 2) {
 
-            if(vez){
+            if (vez) {
                 System.out.println("vez do jogador: " + jogador1.getNome());
                 System.out.println("opcões: ");
                 System.out.println("1- Escolher cava");
                 System.out.println("2- Sair do jogo");
                 opcao = scan.nextInt();
-                if (opcao == 1) {
 
+                if (opcao == 1) {
                     System.out.println("escolha uma cava(1-6): ");
                     cava = scan.nextInt();
                     repeteVez = tabuleiro.semear(cava-1,vez);
                     tabuleiro.mostrarTabuleiro();
                     System.out.println();
-                    if(repeteVez == true){
+
+                    if (repeteVez == true) {
                         System.out.println("parabens, voce pode jogar novamente");
                         repeteVez = false;
                         
-                    }else{
+                    } else {
                         vez = false;
                     }
 
-                    if(tabuleiro.verificaFim(jogador2, jogador1)){
+                    if (tabuleiro.verificaFim(jogador2, jogador1)) {
                         opcao = 2;
                     }
-                       
                 }
-            }else{
+            } else {
                 System.out.println("vez do jogador: " + jogador2.getNome());
                 System.out.println("opcões: ");
                 System.out.println("1- Escolher cava");
                 System.out.println("2- Sair do jogo");
                 opcao = scan.nextInt();
-                if(opcao == 1){
 
-
+                if (opcao == 1) {
                     System.out.println("escolha uma cava(1-6): ");
                     cava = scan.nextInt();
                     repeteVez = tabuleiro.semear(cava+6,vez);
                     tabuleiro.mostrarTabuleiro();
                     System.out.println();
-                    if(repeteVez == true){
+
+                    if (repeteVez == true) {
                         System.out.println("parabens, voce pode jogar novamente");
                         repeteVez = false;
-                        
-                    }else{
+                    } else {
                         vez = true;
                     }
 
-                    if(tabuleiro.verificaFim(jogador1, jogador2)){
+                    if (tabuleiro.verificaFim(jogador1, jogador2)) {
                         opcao = 2;
                     }
-
                 }
             }
-
-
         }
-        
-
-
-
     }
-
 }
